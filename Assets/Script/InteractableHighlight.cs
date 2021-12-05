c5fa2c1c6b15faa3a36c9f5d32caed61dbda0448
assthehole <73062838+assthehole@users.noreply.github.com>
2021-12-05 21:03:16 +0800

    Changes

---
+++ Assets/Script/InteractableHighlight.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableHighlight : MonoBehaviour
+{
+    //highlight the object using emission property
+    public void OnHover()
+    {
+        //get all the mesh renderers
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        //go through all the renderers and turn on the emission property in each material
+        foreach (MeshRenderer renderer in meshRenderers)
+        {
+            //turn on the emission property
+            renderer.material.EnableKeyword("_EMISSION");
+        }
+    }
+    //stop highlight the object
+    public void ExitHover()
+    {
+        //get all the mesh renderers
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        //go through all the renderers and turn on the emission property in each material
+        foreach (MeshRenderer renderer in meshRenderers) 
+        {
+            //turn off the emission property
+            renderer.material.DisableKeyword("_EMISSION");
+        }
+    }
+}
---
+++ Assets/Script/basket.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basket : MonoBehaviour
+{
+    public string AnimalFood;
+    public Animator myanimator;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag + " is inside the basket");
+        //Check if the Object inside is a food if not do something
+        if (other.gameObject.tag == "Food")
+        {
+            Debug.Log("Animation of animal walking to the food....");
+            //set the animation of the animal coming towards the food
+            myanimator.SetBool("Moving", true);
+
+            //wait for the animation to be completed before playing the others
+            StartCoroutine(CheckFood(other.gameObject));           
+        }
+        else
+        {
+            //do something if the game object is not food
+            //Show a UI to Display the Time left to Destory the food
+            //add a timer to destroy the game object? 
+        }
+    }
+
+    private IEnumerator CheckFood(GameObject food)
+    {
+        yield return new WaitForSeconds(3);
+        //Check if the food is the correct food
+        if (food.name == AnimalFood)
+        {
+            //do something when the food is correct
+            food.SetActive(false);
+            //animal will eat the food
+            Debug.Log("Nomm Nommm....");
+        }
+        else
+        {
+            //the animal will turn back
+            myanimator.SetBool("Moving", false);
+        }
+    }
+
+}
