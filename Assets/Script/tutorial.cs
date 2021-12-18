d0cc3aa79a2410f96cf12b23ef0e3a0f1e92411b
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2021-12-18 17:09:02 +0800

    changes

--- Assets/Script/tutorial.cs
+++ Assets/Script/tutorial.cs
@@ -1,23 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class tutorial : MonoBehaviour
-{
-    public bool teleportTaskDone;
-    public bool grabTaskDone;
-    public void Teleported()
-    {
-        Debug.Log("I have teleported");
-        //set the teleportTask to be done
-        teleportTaskDone = true;
-    }
-
-    void Update()
-    {
-        if (teleportTaskDone && grabTaskDone)
-        {
-            Debug.Log("Tutorial Task Complete!");
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2bbbc3a32e5d73490db58fe7f44a9dfd43be3fe2d0ea3a9999c597d982f5e397
+size 489
