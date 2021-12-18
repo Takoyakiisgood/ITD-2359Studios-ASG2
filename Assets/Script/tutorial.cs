8c67ec1e0145531e1bfde68f92c4d402e6ea9f02
Jordan Yeo <73056451+jordanyeo2001@users.noreply.github.com>
2021-12-18 17:20:33 +0800

    changes

--- Assets/Script/tutorial.cs
+++ Assets/Script/tutorial.cs
@@ -1,3 +1,8 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:510dde740e9d1765970f4395064642b7263dcd9e935fd10142093c2499fce1bc
-size 283
+<<<<<<< HEAD
+oid sha256:2bbbc3a32e5d73490db58fe7f44a9dfd43be3fe2d0ea3a9999c597d982f5e397
+size 489
+=======
+oid sha256:df4911711d87ba4cc777014352f369700611406ac36a3e6cf0f455271d2d3d6f
+size 512
+>>>>>>> ca26dacc1300b8b3ca11fb122321ad49fcdcba29
