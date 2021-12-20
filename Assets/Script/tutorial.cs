ba223252e563b3c769f3d56aeeac2da0f22787e0
Ng Hui Ling <68372136+huiling3o3@users.noreply.github.com>
2021-12-20 13:39:27 +0800

    remove mixed reality toolkit

--- Assets/Script/tutorial.cs
+++
@@ -1,8 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-<<<<<<< HEAD
-oid sha256:2bbbc3a32e5d73490db58fe7f44a9dfd43be3fe2d0ea3a9999c597d982f5e397
-size 489
-=======
-oid sha256:df4911711d87ba4cc777014352f369700611406ac36a3e6cf0f455271d2d3d6f
-size 512
->>>>>>> ca26dacc1300b8b3ca11fb122321ad49fcdcba29
